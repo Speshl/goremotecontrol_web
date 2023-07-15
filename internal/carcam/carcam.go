dc34f0ae8cf73b040118ec3ad4b4b2cc06cc2970
Chaz Gregory <speshled@gmail.com>
2023-07-15 11:17:38 -0500

    specify custom

--- internal/carcam/carcam.go
+++ internal/carcam/carcam.go
@@ -74,7 +74,7 @@ func (c *CarCam) startStreaming() {
 
 	videoSrc := "libcamerasrc ! video/x-raw,format=YUY2,height=480,width=640,colorimetry=2:4:5:1,framerate=30/1 ! videoconvert ! v4l2h264enc ! 'video/x-h264,level=(string)3,stream-forrmat=byte-stream,alignment=au,profiile=baseline,width=640,height=480,pixel-aspect-ratio=1/1,colorimetry=bt709,interlace-mode=progressive'" //webcam
 	//videoSrc := "videotestsrc"
-	c.VideoPipeline = gst.CreatePipeline("h264-hardware", []*webrtc.TrackLocalStaticSample{c.VideoTracks[0], c.VideoTracks[1]}, videoSrc)
+	c.VideoPipeline = gst.CreatePipeline("custom", []*webrtc.TrackLocalStaticSample{c.VideoTracks[0], c.VideoTracks[1]}, videoSrc)
 	c.VideoPipeline.Start()
 }
 
