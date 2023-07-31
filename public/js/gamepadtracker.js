5b1cd86cc5c4722f7ebefc9b0987432673b5600f
Chaz Gregory <speshled@gmail.com>
2023-07-30 21:42:48 -0500

    added TGT steering wheel

--- public/js/gamepadtracker.js
+++ public/js/gamepadtracker.js
@@ -37,6 +37,9 @@ class GamePadTracker {
                 command = this.commandFromXbox(myGamepad);
             }else if(myGamepad.id.toLowerCase().includes("g27")){
                 command = this.commandFromG27(myGamepad);
+            }
+            else if(myGamepad.id.toLowerCase().includes("B684")){
+                command = this.commandFromTGT(myGamepad);
             }else{
                 document.getElementById('controllerType').innerHTML = "Unsupported - " + myGamepad.id;
             }
@@ -90,6 +93,100 @@ class GamePadTracker {
                 this.steeringTrim++;
             }
         }else if (myGamepad.buttons[15] == 0 && this.trimRightPress == true){
+            this.trimRightPress = false;
+        }
+        
+        if(steerCommand + this.steeringTrim > 255){
+            steerCommand = 255;
+        }else if(steerCommand + this.steeringTrim < 0) {
+            steerCommand = 0;
+        }else{
+            steerCommand = steerCommand + this.steeringTrim;
+        }
+        command[1] = steerCommand;
+    
+        //dpad    Pan/Tilt
+        dpadValue = myGamepad.axes[9].toFixed(2)
+        const upkey = -1.00
+        const uprightkey = -0.71429
+        const rightkey = -0.42857
+        const downrightkey = -0.14286
+        const downkey = 0.14286
+        const downleftkey = 0.42857
+        const leftkey = 0.71429
+        const upleftkey = 1.00
+    
+        if(dpadValue == upkey.toFixed(2)){
+            //up
+            command[3] = 255;
+        }else if(dpadValue == uprightkey.toFixed(2)){
+            //up-right
+            command[3] = 255;
+            command[2] = 255;
+        }else if(dpadValue == rightkey.toFixed(2)){
+            //right
+            command[2] = 255;
+        }else if(dpadValue == downrightkey.toFixed(2)){
+            //down-right
+            command[2] = 255;
+            command[3] = 0;
+        }else if(dpadValue == downkey.toFixed(2)){
+            //down
+            command[3] = 0;
+        }else if(dpadValue == downleftkey.toFixed(2)){
+            //down-left
+            command[3] = 0;
+            command[2] = 0;
+        }else if(dpadValue == leftkey.toFixed(2)){
+            //left
+            command[2] = 0;
+        }else if(dpadValue == upleftkey.toFixed(2)){
+            //up-left
+            command[2] = 0;
+            command[3] = 255;
+        }
+        
+        return command;
+    }
+
+    commandFromTGT(myGamepad) {
+        let command = this.neutralCommand;
+        //esc
+        if(myGamepad.axes[2] < .9){
+            command[0] = this.mapToRange(myGamepad.axes[5], -1, 1, 127, 255);
+        }else if(myGamepad.axes[5] < .9){
+            command[0] = this.mapToRange(myGamepad.axes[1], -1, 1, 0, 127);
+        }else{
+            command[0] = 127;
+        }
+    
+        //servo
+        let steerCommand = command[1];
+        if(myGamepad.axes[0] > .05){
+            steerCommand = this.mapToRange(myGamepad.axes[0], .05, 1, 127, 255);
+        }else if(myGamepad.axes[0] < -.05){
+            steerCommand = 127 - this.mapToRange(myGamepad.axes[0], -.05, -1, 0, 127);
+        }else{
+            steerCommand = 127;
+        }
+
+        //steering trim
+        if(myGamepad.buttons[14].pressed  && this.trimLeftPress == false){ //new press
+            this.trimLeftPress = true;
+            this.steeringTrim--;
+            if(this.steeringTrim > this.minTrim){
+                this.steeringTrim--;
+            }
+        }else if (!myGamepad.buttons[14].pressed  && this.trimLeftPress == true){
+            this.trimLeftPress = false;
+        }
+
+        if(myGamepad.buttons[3].pressed  && this.trimRightPress == false){ //new press
+            this.trimRightPress = true;
+            if(this.steeringTrim < this.maxTrim){
+                this.steeringTrim++;
+            }
+        }else if (!myGamepad.buttons[15].pressed  && this.trimRightPress == true){
             this.trimRightPress = false;
         }
         
