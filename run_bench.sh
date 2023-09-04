4d7737dad91f54e4fafbda219c8e1fae202c0463
Chaz Gregory <speshled@gmail.com>
2023-09-03 21:38:05 -0500

    bench env

---
+++ run_bench.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+file="./goremotecontrol_web"
+
+if [ -f "$file" ] ; then
+    rm "$file"
+fi
+
+echo Compiling...
+go build .
+
+export $(grep -v '^#' bench_car.env | xargs)
+export XDG_RUNTIME_DIR=""
+
+sudo pactl load-module module-echo-cancel source_master=alsa_input.usb-C-Media_Electronics_Inc._USB_PnP_Sound_Device-00.mono-fallback aec_method=webrtc source_name=echocancel sink_name=echocancel1
+
+sudo -E ./goremotecontrol_web
