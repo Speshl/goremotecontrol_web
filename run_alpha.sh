5bf9b25de19b1cbbdff248b5ae122fd18f78e7e4
Chaz Gregory <speshled@gmail.com>
2023-09-05 22:05:20 -0500

    remove echo cancel

--- run_alpha.sh
+++ run_alpha.sh
@@ -12,6 +12,4 @@ go build .
 export $(grep -v '^#' alpha_car.env | xargs)
 export XDG_RUNTIME_DIR=""
 
-sudo pactl load-module module-echo-cancel source_master=alsa_input.usb-C-Media_Electronics_Inc._USB_PnP_Sound_Device-00.mono-fallback aec_method=webrtc source_name=echocancel sink_name=echocancel1
-
 sudo -E ./goremotecontrol_web
